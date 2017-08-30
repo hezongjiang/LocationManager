df2ad0f81739f42a158b232a8200ce274757f33e
he <he@Hello-World.local>
2017-08-30 10:52:31 +0800

    demo完善

--- LocationDemo/ViewController.swift
+++ LocationDemo/ViewController.swift
@@ -7,21 +7,33 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
 
+    fileprivate lazy var geocoder: CLGeocoder = CLGeocoder()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+       
         LocationManager.shared.startUpdatingLocation(atOnce: false) { (location, status, error) in
-            print(location)
-            print(status.rawValue)
-        }
         
+            if status == CLAuthorizationStatus.notDetermined {
+                print("用户还未决定是否能定位")
+                return
+            } else if status == CLAuthorizationStatus.denied || status == CLAuthorizationStatus.restricted {
+                print("定位功能不可用")
+                return
+            } else if let location = location {
+                print(location)
+            }
+            
+        }
     }
 
 
